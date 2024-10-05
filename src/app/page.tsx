902b1ea469b91fdab9f04dbf2660dfc88f42cf64
aldoprogrammer <aldobesma@gmail.com>
2024-10-05 17:14:00 +0700

    init app and login register

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from "react";
+import Home from "./components/Home";
+
+export default function Main() {
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      <Home />
+    </main>
+  );
+}
+
+
